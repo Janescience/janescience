838f806a5ce83d65ea69c42c4b175de4e9e5b18a
janwit.si <janwit.si@ocean.co.th>
2022-04-18 09:50:36 +0700

    Fix prettier

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -3,9 +3,7 @@ import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 
 export default function AuthorLayout({ children, frontMatter }) {
-  const { name, avatar, occupation, company 
-    // email, twitter, linkedin, github 
-  } = frontMatter
+  const { name, avatar, occupation, company } = frontMatter
 
   return (
     <>
