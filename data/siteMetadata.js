17ed640589c7754c4854f2d1acb4e807dbcd4310
janescience <janescience.top@gmail.com>
2023-02-09 18:27:52 +0700

    update google analyze id

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -25,7 +25,7 @@ const siteMetadata = {
     plausibleDataDomain: 'janescience.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: true, // true or false
     umamiWebsiteId: '123e4567-e89b-12d3-a456-426614174000', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'UA-000000-2', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-T6CB8CD7TF', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue
