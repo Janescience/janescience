630ff692d16b5b340cf8a317bf95293546737225
janescience <janescience.top@gmail.com>
2023-02-09 10:41:44 +0700

    update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,10 +22,10 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    plausibleDataDomain: 'janescience.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: true, // true or false
+    umamiWebsiteId: '123e4567-e89b-12d3-a456-426614174000', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: 'UA-000000-2', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue
