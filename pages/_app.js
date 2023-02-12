234d6683b91a8a5288ffbfee3faa1d85df3d7500
janwit.si <janescience.top@gmail.com>
2023-02-12 23:28:38 +0700

    add ga on head

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,7 @@ import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
-import Footer from '@/components/Footer'
 import { ClientReload } from '@/components/ClientReload'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
@@ -23,7 +21,6 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      {/* <Analytics /> */}
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
